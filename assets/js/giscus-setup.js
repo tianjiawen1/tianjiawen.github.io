7929a160945f57a41590cad9c5ff3291b8b407dd
tianjiawen1 <97448664+tianjiawen1@users.noreply.github.com>
2025-09-25 03:58:29 +0000

    Deploying to gh-pages from @ tianjiawen1/tianjiawen1.github.io@3b5e7952fdde25770b920375a711eec041a74be8 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "tianjiawen1/tianjiawen.github.io",
+    "data-repo": "tianjiawen1/tianjiawen1.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
